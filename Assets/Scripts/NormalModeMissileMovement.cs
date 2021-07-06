c2d2c692548f91b219bed887253275f069d093a3
kunwithgithub <kunhuang@ucdavis.edu>
2021-07-05 22:56:15 -0700

    update feature for rigibody sync

--- Assets/Scripts/NormalModeMissileMovement.cs
+++ Assets/Scripts/NormalModeMissileMovement.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MissleFactory;
+using Photon.Pun;
 
 public class NormalModeMissileMovement : AbstractMissileMovement
 {
+    private PhotonView PV;
+    private Rigidbody RB;
     void Awake()
     {
         this.Speed = 100.0f;
+        this.PV = GetComponent<PhotonView>();
+        this.RB = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        if(!PV.IsMine)
+        {
+            Destroy(this.RB);
+        }
+
     }
     public override void Fire(Transform rotation)
     {
         //this.GetComponent<Rigidbody>().AddForce(this.FireDirection*this.Speed);
 
         // Debug.Log(this.FireDirection*this.Speed);
-        var projectileRigidBody = this.GetComponent<Rigidbody>();
+        var projectileRigidBody = this.RB;
         Vector3 correctingVector = new Vector3(1.0f, 0.0f, 1.0f);
         Vector3 test = new Vector3();
         test = Vector3.Scale(correctingVector, this.transform.forward);
