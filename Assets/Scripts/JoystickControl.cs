15f50d4c545196dbbbee9b40ce7e50c6018ae985
kunwithgithub <kunhuang@ucdavis.edu>
2021-08-28 21:33:04 -0700

    add functionality to rotate the joystick

---
+++ Assets/Scripts/JoystickControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickControl : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    private Vector3 JoystickEulerAngles;
+    void Awake()
+    {
+        this.JoystickEulerAngles = this.transform.rotation.eulerAngles;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(this.xAngle,this.yAngle,this.zAngle));
+    }
+}
