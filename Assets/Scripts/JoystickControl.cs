16c964d63ce8b065579829a757d711b1034bd829
unknown <quadcopterar2306@gmail.com>
2021-09-25 18:29:03 -0700

    update xr controls for quadcopter cockpit joysticks

--- Assets/Scripts/JoystickControl.cs
+++ Assets/Scripts/JoystickControl.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 public class JoystickControl : MonoBehaviour
 {
+
+    // oculus setup
+    public bool debugMode = false;
+    private XRNode leftControllerNode = XRNode.LeftHand;
+    private List<UnityEngine.XR.InputDevice> leftInputDevices = new List<UnityEngine.XR.InputDevice>();
+    private UnityEngine.XR.InputDevice leftController;
+    private XRNode rightControllerNode = XRNode.RightHand;
+    private List<UnityEngine.XR.InputDevice> rightInputDevices = new List<UnityEngine.XR.InputDevice>();
+    private UnityEngine.XR.InputDevice rightController;
+
+    // joystick control setup
     public float xAngle, yAngle, zAngle;
+    [RequireComponent(typeof(bool))]
+    public bool leftStickTrueRightStickFalse;
     public string KeyUp, KeyDown, KeyLeft, KeyRight; 
     private float lerpTimer;
     public float stepSize;
@@ -103,26 +118,40 @@ public class JoystickControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // oculus joystick connection
+        if(!debugMode){
+            if (leftController == null) {
+                GetControllerDevices(leftControllerNode, ref leftController, ref leftInputDevices);
+            }
 
-        if(Input.GetKey(this.KeyUp))
-        {
-            this.RollToFront();
+            if (rightController == null) {
+                GetControllerDevices(rightControllerNode, ref rightController, ref rightInputDevices);
+            }
+            // content for joystick motion detection
+            CheckForChanges();
         }
 
-        if(Input.GetKey(this.KeyDown))
-        {
-            this.RollToRear();
-        }
 
-        if(Input.GetKey(this.KeyLeft))
-        {
-            this.RollToLeft();
-        }
 
-        if(Input.GetKey(this.KeyRight))
-        {
-            this.RollToRight();
-        }
+        // if(Input.GetKey(this.KeyUp))
+        // {
+        //     this.RollToFront();
+        // }
+
+        // if(Input.GetKey(this.KeyDown))
+        // {
+        //     this.RollToRear();
+        // }
+
+        // if(Input.GetKey(this.KeyLeft))
+        // {
+        //     this.RollToLeft();
+        // }
+
+        // if(Input.GetKey(this.KeyRight))
+        // {
+        //     this.RollToRight();
+        // }
 
         if(!Input.anyKey)
         {
@@ -133,6 +162,62 @@ public class JoystickControl : MonoBehaviour
 
         this.gameObject.transform.rotation = Quaternion.Lerp(this.gameObject.transform.rotation, Quaternion.Euler(this.XAngle,this.YAngle,this.ZAngle), this.stepSize * Time.deltaTime);
     }
+
+
+    void CheckForChanges() {
+        Vector2 leftTouchCoords;
+        Vector2 rightTouchCoords;
+        bool triggerVal;
+        if(leftStickTrueRightStickFalse)
+        {
+            if (leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out leftTouchCoords) && leftTouchCoords != Vector2.zero)
+            {
+
+                if(leftTouchCoords.x < -deadZoneAmount){
+                //   MoveLeft(leftTouchCoords.x);
+                    this.RollToLeft();
+                }else if(leftTouchCoords.x > deadZoneAmount){
+                //   MoveRight(leftTouchCoords.x);
+                    this.RollToRight();
+                }
+
+                if (leftTouchCoords.y < -deadZoneAmount) {
+                    // MoveBackward(leftTouchCoords.y);
+                    this.RollToRear();
+                } else if (leftTouchCoords.y > deadZoneAmount) {
+                    // MoveForward(leftTouchCoords.y);
+                    this.RollToFront();
+                }
+            }
+        }
+        else
+        {
+            if (rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out rightTouchCoords) && rightTouchCoords != Vector2.zero)
+            {
+                if(rightTouchCoords.x < -deadZoneAmount){
+                //   RotateLeft(rightTouchCoords.x);
+                    this.RollToLeft();
+                }else if(rightTouchCoords.x > deadZoneAmount){
+                //   RotateRight(rightTouchCoords.x);
+                    this.RollToRight();
+                }
+
+                if (rightTouchCoords.y < -deadZoneAmount) {
+                //   MoveDown(rightTouchCoords.y);
+                    this.RollToRear();
+                } else if (rightTouchCoords.y > deadZoneAmount) {
+                //   MoveUp(rightTouchCoords.y);
+                    this.RollToFront();
+                }
+            }
+ 
+        }
+
+
+
+    }
+
+
 
     void RollToLeft()
     {
